68a918f5bcb88606c015eb2e815c9fe7bbdd50f6
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-06-30 17:02:15 +0200

    ahex: segmentation fault, all is destroyed, the end of the world is near

--- ahex.asm
+++ ahex.asm
@@ -13,22 +13,22 @@
 %include "start.inc.asm"
 %include "ahex.inc.asm"
 
-; ----------------------------------------------------------------------
-%macro fillbuffer 3
-	sys_read 	%1, %2, %3
-	cmp			rax, 0
-%endmacro
+
+; %define HEXSTOSB
 
 ; ----------------------------------------------------------------------
 ENTRY_POINT
 
 main_loop:
 	;§g fill buffer_in from STDIN
-	fillbuffer 	STDIN, buffer_in, buffer_size
+	sys_read 	STDIN, buffer_in, buffer_size
+	cmp			rax, 0
 	;§g if rax == 0, no more bytes to read
 	jz			end_loop
 	;§g if rax < 0, it's an error
 	jb			onerror
+	; r14 <- number of read bytes
+	mov			r14, rax
 	;§g fill the line to put on STDOUT
 	call		fillline
 	;§g write the line
@@ -42,8 +42,18 @@ end_loop:
 onerror:
 	sys_exit 	rax
 
+; -----------------------------------------------
 	; filling the line
 fillline:
+
+	; push		r14
+	; mov			rdi, bytes1
+	; call		reset_bytes_1_2
+	; mov			rdi, bytes2
+	; call		reset_bytes_1_2
+	call		reset_chars
+	; pop			r14
+
 	; rdi points on line
 	mov			rdi, line
 	; we process only 8 nibbles of the address
@@ -55,18 +65,25 @@ fillline:
 	pop			r10
 	add			r10d, buffer_size
 	mov			[address], r10d
+	ret
 
+	push		r14
 	mov			rsi, buffer_in
 	mov			rdi, bytes1
 	call		loopbytes
 	mov			rdi, bytes2
 	call		loopbytes
+	pop			r14
 
 	mov			rsi, buffer_in
 	mov			rdi, chars
 	mov			rcx, buffer_size
+	cmp			rcx, r14
+	jnb			loopchars
+	mov			rcx, r14
+	cld
 loopchars:
-	mov			al, [rsi]
+	lodsb
 	cmp			al, 020h
 	jb			badchars
 	cmp			ax, 128
@@ -74,14 +91,13 @@ loopchars:
 badchars:
 	mov			al, '.'
 savechar:
-	mov			[rdi], al
-	inc			rsi
-	inc			rdi
+	stosb
 	loop		loopchars
 
 end_fill_line:
 	ret
 
+; -----------------------------------------------
 ;	r10: value in input
 ;	rcx : number of output chars
 ;	rdi : address of the receiving string
@@ -95,17 +111,24 @@ tohex:
 	; => rdi += rcx - 1
 	add			rdi, rcx
 	dec			rdi
+%ifdef HEXSTOSB
+	std
+%endif
 looptohex:
 	; r11 <- input value
 	mov			r11, r10
 	; mask r11 to keep only the lowest nible
 	and 		r11, 0fh
-	; r12 byte <- hexsymbols[r11]
-	mov			r12b, [rsi + r11]
-	; put r12 byte in  the string
-	mov			[rdi], r12b
+	; al byte <- hexsymbols[r11]
+	mov			al, [rsi + r11]
+%ifdef HEXSTOSB
+	stosb
+%else
+	; put al byte in  the string
+	mov			[rdi], al
 	; rdi points on previous byte
 	dec			rdi
+%endif
 	; r10 <- a shift to get the next 4 bits
 	shr			r10, 4
 	; loop
@@ -116,9 +139,15 @@ looptohex:
 	; return to caller
 	ret
 
+; -----------------------------------------------
+	; rsi: pointer on buffer_in
+	; rdi: pointer on bytes1/2
 loopbytes:
 	mov 		rcx, buffer_size
 	shr			rcx, 1
+	; cmp			r14, rcx
+	; ja			inner_loopbytes
+	; mov			rcx, r14
 inner_loopbytes:
 	push		rcx
 	mov			rcx, 2
@@ -130,3 +159,23 @@ inner_loopbytes:
 	loop		inner_loopbytes
 	ret
 
+
+; -----------------------------------------------
+	; rdi: pointer on bytes1/2
+reset_bytes_1_2:
+	mov			rcx, buffer_size
+	add			rcx, buffer_size
+	add			rcx, buffer_size
+	mov			al, ' '
+	cld
+rep	stosb
+	ret
+
+; -----------------------------------------------
+reset_chars:
+	mov			rdi, chars
+	mov			cx, buffer_size
+	mov			al, ' '
+	cld
+rep	stosb
+	ret
